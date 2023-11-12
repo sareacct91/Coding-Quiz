1c4ff30a17045bb07b004e19b51e165325e8cb30
Meow Roar <sareacct91@gmail.com>
2023-11-11 19:11:39 -0800

    Added functionalities go to new question after selection subtract time for wrong answer

--- assets/Scripts/script.js
+++ assets/Scripts/script.js
@@ -1,3 +1,4 @@
+// Declarations
 const startingCard = document.querySelector("#startingCard");
 const questionCard = document.querySelector("#questionCard");
 
@@ -5,12 +6,27 @@ const timeDisplay = document.querySelector("#timeDisplay");
 
 const startBtn = document.querySelector("#startBtn");
 const questionDisplay = document.querySelector("#questionDisplay");
-const answerBtn1 = document.querySelector("#answerBtn1");
-const answerBtn2 = document.querySelector("#answerBtn2");
-const answerBtn3 = document.querySelector("#answerBtn3");
-const answerBtn4 = document.querySelector("#answerBtn4");
 
+const buttonArr = [
+  document.querySelector("#answerBtn1"),
+  document.querySelector("#answerBtn2"),
+  document.querySelector("#answerBtn3"),
+  document.querySelector("#answerBtn4")
+];
+
+let index = 0;
+let questionList = [];
 let timer = {second: 600, intervalId: null};
+
+// Functions
+// Function to fetch questions array in questions.json
+async function startQuiz() {
+  const response = await fetch("./assets/Scripts/questions.json");
+  questionList = await response.json();
+    
+  renderQuestion();
+}
+startQuiz();
 
 function starTimer() {
   timeDisplay.innerHTML = `Time: ${timer.second}`;
@@ -21,33 +37,37 @@ function starTimer() {
   }, 1000);
 }
 
-
-function renderQuestion(questionPar) {
-  questionDisplay.innerHTML = questionPar.question;
-  answerBtn1.innerHTML = questionPar.ans1;
-  answerBtn2.innerHTML = questionPar.ans2;
-  answerBtn3.innerHTML = questionPar.ans3;
-  answerBtn4.innerHTML = questionPar.ans4;
-}
-
-async function startQuiz() {
-  let currentQ;
-
-  const response = await fetch("questions.json");
-  let questionList = await response.json();
-  
-  starTimer();
-  for (let i = 0; i < questionList.length; i++) { 
-    currentQ = questionList[i];
+function renderQuestion() {
+  if (index < questionList.length) {
+    questionDisplay.innerHTML = questionList[index].question;
+    answerBtn1.innerHTML = questionList[index].ans1;
+    answerBtn2.innerHTML = questionList[index].ans2;
+    answerBtn3.innerHTML = questionList[index].ans3;
+    answerBtn4.innerHTML = questionList[index].ans4;
+  } else {
     
-    renderQuestion(currentQ);
   }
-  
-  
 }
+
+function checkAnswer(answerStr) {
+  if (answerStr !== questionList[index].correct) {
+    timer.second -= 120;    
+  }
+  // show footer
+  index++;
+  renderQuestion();
+}
+
+
 
 startBtn.addEventListener("click",() =>{
-  startQuiz();
   startingCard.classList.add("isNotDisplay");
   questionCard.classList.remove("isNotDisplay");
+  starTimer();
 } );
+
+buttonArr.forEach((element,i) => {
+  element.addEventListener("click", () => {
+    checkAnswer(`ans${i+1}`);
+  });
+});
